a4d06d2c6c90f75f837de4a0af97d7696733b710
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-07-15 15:00:20 -0400

    Adding pLaces view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -7,17 +7,16 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: 'http://placekitten.com/250/250/?image=2'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: 'http://placekitten.com/250/250/?image=10'
       }]      
     res.render('places/index', { places } )
 })
-
 
 
 module.exports = router
--- index.js
+++ index.js
@@ -13,6 +13,31 @@ app.use(express.static('public'))
 // Controllers and routes
 app.use('/places', require('./controllers/places'))
 
+// GET Homwe page
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+        }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+        }]
+
+    res.render('places/index', { places })
+})
+
+
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -22,4 +47,5 @@ app.get('*', (req, res)  => {
 })
 
 const PORT = process.env.PORT
+
 app.listen(PORT, console.log(`Listening on port ${PORT}`))
