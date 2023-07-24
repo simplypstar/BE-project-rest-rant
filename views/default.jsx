1f3292f4f163f653d6d27d777fa31d87f067a6c9
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-07-24 01:35:05 -0400

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -10,6 +10,13 @@ function Def (html) {
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/places">Places</a></li>
+                        <li><a href="/places/new">Add a Place</a></li>
+                    </ul>
+                </nav>
                 {html.children}
 
                 {/* <p></p><p><h3>.</h3></p> */}
