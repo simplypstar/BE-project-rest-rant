8fcf1f05dbadc4a948148cd5c176ddc86e41a6c5
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-07-30 20:07:24 -0400

    added seeder script

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,26 +9,30 @@ function show (data) {
                     <div className='showRows'>
                         <div className='item1'>
                             <img src={data.place.pic} alt={data.place.name}/>
+                            <h3>Located in {data.place.city}, {data.place.state}</h3>
                         </div>
                         <div className='item2'>
                             <h1>{ data.place.name }</h1>                           
                             <h2>Rating</h2>
                             <h5>Not Rated</h5>
                             <h2>Description</h2>
-                            <h5>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h5>
+                            {/* <h5>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h5> */}
+                            <h3>{data.place.showEstablished()}</h3>
+                            <h4>Serving {data.place.cuisines}</h4>
+                            {/* <p></p> */}
+                            {/* <a href={`places/${data.id}/edit`} className='btn btn-warning'>Edit</a> */}
+
+                            <button href={`/places/${data._id}/edit`} className="btn btn-warning"> Edit</button>
+                            
+                            <form method="POST" action={`/places/${data._id}?_method=DELETE`}> 
+                                <button type="submit" className="btn btn-danger">
+                                    Delete
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div></center> 
-                <p></p>
-                <a href={`places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
-
-                <button href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</button>
-                 
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
+                
                 <hr></hr>
                 <h1>Comments</h1>
                 <h5>No comments yet!</h5>
